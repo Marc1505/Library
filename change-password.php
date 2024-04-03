3523aa000a601a2028de6d3438c2a9cc12d71209
Marc1505 <m.annassamy@ecole-ipssi.net>
2024-04-03 09:43:44 +0200

    first commit

--- change-password.php
+++ change-password.php
@@ -24,22 +24,22 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
      	<label>Ancien Mot de Passe</label>
      	<input type="password" 
      	       name="op" 
-     	       placeholder="Old Password">
+     	       placeholder="Ancien Mot de Passe">
      	       <br>
 
      	<label>Nouveau Mot de Passe</label>
      	<input type="password" 
      	       name="np" 
-     	       placeholder="New Password">
+     	       placeholder="Nouveau Mot de Passe">
      	       <br>
 
      	<label>Confirmer le Mot de Passe</label>
      	<input type="password" 
      	       name="c_np" 
-     	       placeholder="Confirm New Password">
+     	       placeholder="Confirmer le Mot de Passe">
      	       <br>
 
-     	<button type="submit">CHANGE</button>
+     	<button type="submit">Changez</button>
           <a href="index.php" class="ca">HOME</a>
      </form>
 </body>
